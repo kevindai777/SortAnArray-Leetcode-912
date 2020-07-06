f7dd75a0f3edaf6e2d90ffa360ec833dd2c47353
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-06 03:10:48 -0400

    solutions

--- sortanarray.js
+++ sortanarray.js
@@ -11,18 +11,22 @@ function mergeSort(list) {
         return list
     }
 
+    //Find the middle of the array, then split the array into two
+    //and keep splitting until we have single elements
     let mid = Math.floor(list.length / 2)
     let left = mergeSort(list.slice(0, mid))
     let right = mergeSort(list.slice(mid, list.length))
 
+    //Once we're done splitting, merge everything back together
     return merge(left, right)
 
+    //Merge function is to find the lower between two elements then push
+    //it into the result. Then at the end, we concat the two splits together
     function merge(left, right) {
         let result = []
         while (left.length && right.length) {
             result.push(left[0] < right[0] ? left.shift() : right.shift())
         }
-
         return result.concat(left, right)
     }
 }
@@ -37,10 +41,13 @@ function quickSort(list) {
         return list
     }
 
+    //Use a random pivot - the first element in this case
     let pivot = list[0]
     let left = []
     let right = []
 
+    //Put all elements less than and greater than the pivot
+    //into two arrays
     for (let i = 1; i < list.length; i++) {
         if (pivot >= list[i]) {
             left.push(list[i])
@@ -49,6 +56,7 @@ function quickSort(list) {
         }
     }
 
+    //Sort the left side again, then add on the pivot and right array
     return quickSort(left).concat(pivot, right)
 }
-return nums
+return quickSort(nums)
